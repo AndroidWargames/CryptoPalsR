04f4013e34ec84cde6c03c692e3e65308eac9baa
AndroidWargames <adwagner89@gmail.com>
2016-09-13 22:30:29 -0500

    Delete ui.R

--- 4.31/ui.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-
-shinyUI(bootstrapPage(
-        h3("URL components"),
-        verbatimTextOutput("urlText"),
-
-        h3("Parsed query string"),
-        verbatimTextOutput("queryText")
-))
